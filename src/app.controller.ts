12989c07681a5903c1d82ab4fd0a23aad660b66b
ase <irish@fyb-technologies.com>
2025-03-17 19:21:21 +0800

    feat: set up aws sqs app

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ProducerService } from './producer/producer.service';
 
-@Controller()
+@Controller('messages')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly producerService: ProducerService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('send')
+  produceMessage() {
+    this.producerService.sendMessage();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,36 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SqsModule } from '@ssut/nestjs-sqs';
+import { ProducerService } from './producer/producer.service';
+import { ConsumerService } from './consumer/consumer.service';
+import { SQSClient } from '@aws-sdk/client-sqs';
+
+require('dotenv').config();
+
+const sqsClient = new SQSClient({
+  region: (process.env.AWS_DEFAULT_REGION as string),
+  credentials: {
+    accessKeyId: (process.env.AWS_ACCESS_KEY_ID as string),
+    secretAccessKey: (process.env.AWS_SECRET_ACCESS_KEY as string)
+  }
+});
 
 @Module({
   imports: [
-    SqsModule
+    SqsModule.register({
+      consumers: [{
+        name: "consumer1",
+        queueUrl: (process.env.SQS_QUEUE_URL as string),
+        sqs: sqsClient
+      }],
+      producers: [{
+        name: "producer1",
+        queueUrl: (process.env.SQS_QUEUE_URL as string),
+        sqs: sqsClient
+      }]
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ProducerService, ConsumerService],
 })
 export class AppModule {}
