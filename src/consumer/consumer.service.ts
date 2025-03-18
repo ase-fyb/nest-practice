64e6882039f3a8bf0c536c82f12e38d8f94e07bd
ase <irish@fyb-technologies.com>
2025-03-18 11:57:57 +0800

    feat: log processing message errors

--- src/consumer/consumer.service.ts
+++ src/consumer/consumer.service.ts
@@ -1,12 +1,23 @@
-import { Injectable } from '@nestjs/common';
-import { SqsMessageHandler } from '@ssut/nestjs-sqs';
+import { Injectable, Logger } from '@nestjs/common';
+import { SqsConsumerEventHandler, SqsMessageHandler } from '@ssut/nestjs-sqs';
 import { Message } from '@ssut/nestjs-sqs/dist/sqs.types';
 
 @Injectable()
 export class ConsumerService {
+
+  private readonly logger = new Logger(ConsumerService.name);
+
   @SqsMessageHandler('consumer1')
   public async consumeMessage(message: Message) {
     // Message deletion is automatically handled by sqs-consumer after returning the message below
     return 'Received message with body: ' + (await message.body);
+  }
+
+  @SqsConsumerEventHandler("consumer1", "processing_error",)
+  public onProcessingError(error: Error, message: Message) {
+    this.logger.log('Error while processing message:')
+    this.logger.log(error)
+    this.logger.log('Encountered while processing message:')
+    this.logger.log(message)
   }
 }
