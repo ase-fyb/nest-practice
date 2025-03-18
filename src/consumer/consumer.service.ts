3c853a091f7c5a9df55ea966e25f1f2f0179f614
ase <irish@fyb-technologies.com>
2025-03-18 11:53:41 +0800

    docs: add note about message deletion

--- src/consumer/consumer.service.ts
+++ src/consumer/consumer.service.ts
@@ -6,6 +6,7 @@ import { Message } from '@ssut/nestjs-sqs/dist/sqs.types';
 export class ConsumerService {
   @SqsMessageHandler('consumer1')
   public async consumeMessage(message: Message) {
+    // Message deletion is automatically handled by sqs-consumer after returning the message below
     return 'Received message with body: ' + (await message.body);
   }
 }
